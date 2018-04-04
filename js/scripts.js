19e072db477ae1aae833be5d4826e72d2faceea7
Joanna Buchała <joanna.b2@op.pl>
2018-04-04 18:51:19 +0200

    modify scripts

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,11 @@ var triangle3Area = getTriangleArea(3, 6);
 
 
 function getTriangleArea(a, h) {
-	if ( (a <= 0) || (h <= 0) ) {
+	if (a <= 0 || h <= 0)  {
 		 console.log("Incorrect data");
-	} else {
-		return (a * h / 2);
 	}
+
+	return (a * h / 2);
 }
 
 console.log(getTriangleArea(10, 6));
