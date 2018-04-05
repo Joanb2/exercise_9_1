96b93f296d65ebf9241b7b804eebe5a0bc159a37
Joanna Buchała <joanna.b2@op.pl>
2018-04-05 19:03:39 +0200

    correction

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 var triangle1Area = getTriangleArea(7, 5);
-var triangle2Area = getTriangleArea(6, 9);
+var triangle2Area = getTriangleArea(6, 0);
 var triangle3Area = getTriangleArea(3, 6);
 
 
 function getTriangleArea(a, h) {
 	if (a <= 0 || h <= 0)  {
-		 console.log("Incorrect data");
+		return ("Incorrect data");
 	}
 
 	return (a * h / 2);
